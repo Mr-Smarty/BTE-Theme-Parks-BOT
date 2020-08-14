4ae0750778f9da3a07dbf77d8e0c40c8b94dc0c5
Mr-Smarty <meestersmartey@gmail.com>
2020-08-14 18:04:47 -0400

    Initial Project Commit

---
+++ main.js
@@ -0,0 +1,125 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const ping = require('minecraft-server-util');
+
+const ids = require("./ids.json");
+const info = require("./info.json");
+
+client.on("ready", () => {
+    console.log("BTE: Theme Parks BOT is online!");
+});
+
+const prefix = config.prefix;
+
+client.on("message", (message) => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    if (command === 'ping') {
+        message.channel.send("pong!"); 
+    } else
+    if (command === 'smarty') {
+        message.channel.send("The best staff. ~~2nd to Adam~~");
+    } else
+    if (command === 'devcommand' && message.author.id == config.ownerID) {
+        message.channel.send("dev");
+    } else
+    if (command === 'admincommand' && message.member.roles.cache.has(ids.adminRoleID)) {
+        message.channel.send("admin");
+    } else
+    if (command === 'modcommand' && message.member.roles.cache.has(ids.modRoleID)) {
+        message.channel.send("mod");
+    } else
+    if (command === 'modcommand' && message.member.roles.cache.has(ids.trialModRoleID)) {
+        message.channel.send("mod");
+    } else
+    if (command === 'server') {
+        ping('74.99.66.137', 25565, (error, response) => {
+            if (error) {
+                const errorEmbed = new Discord.MessageEmbed()
+                .setTitle('Server Status')
+                .setColor(info.embedHexcode)
+                .setDescription('Offline :pensive: Contact Cdub#5909')
+                .setTimestamp()
+                message.channel.send(errorEmbed);
+            }
+            
+            const serverEmbed = new Discord.MessageEmbed()
+            .setTitle('Server Status')
+            .setColor(info.embedHexcode)
+            .setDescription('Online! :thumbsup:')
+            .addField('Server IP', response.host)
+            .addField('Server Version', response.version)
+            .addField('Online Players', response.onlinePlayers + '\n' + '\n Use `=players` to view online players')
+            .addField('Server Description', response.descriptionText)
+            .setTimestamp()
+            message.channel.send(serverEmbed);
+         
+            console.log(response);
+        });
+    } else
+    if (command === 'commands') {
+        const commandEmbed = new Discord.MessageEmbed()
+        .setTitle("Available Commands")
+        .setColor(info.embedHexcode)
+        .setDescription("**=ping** \n **=DevCommand** (TEST) \n **=AdminCommand** (TEST) \n **=ModCommand** (TEST) \n **=server**  \n > =players \n **=commands**")
+        .setTimestamp(info.commandUpdate)
+        .setFooter("Last updated by MrSmarty#1732", info.devIconLink)
+        message.channel.send(commandEmbed);
+    } else
+    if (command === 'players') {
+        ping('74.99.66.137', 25565, (error, response) => {
+            if (error) {
+                const errorEmbed = new Discord.MessageEmbed()
+                .setTitle('Server Status')
+                .setColor(info.embedHexcode)
+                .setDescription('Offline :pensive: Contact Cdub#5909')
+                .setTimestamp()
+                message.channel.send(errorEmbed);
+            }
+            
+            if (response.samplePlayers === null) {
+                if (response.onlinePlayers === 0) {
+                    const noPlayerEmbed = new Discord.MessageEmbed()
+                    .setTitle('Online Players')
+                    .setColor(info.embedHexcode)
+                    .setDescription('No players are online :confused:')
+                    .setTimestamp()
+                    message.channel.send(noPlayerEmbed);  
+                } else {
+                    const manyPlayersEmbed = new Discord.MessageEmbed()
+                    .setTitle('Online Players')
+                    .setColor(info.embedHexcode)
+                    .setDescription("There's too many players to list! :open_mouth:")
+                    .setTimestamp()
+                    message.channel.send(manyPlayersEmbed);
+                }
+            } else {
+                let players = [];
+                let x = 0;
+                let max = response.samplePlayers.length;
+
+                while ((x + 1) <= max) {
+                    players[x] = response.samplePlayers[x].name;
+                    x++;
+                }
+                console.log(players)
+
+                const playerEmbed = new Discord.MessageEmbed()
+                .setTitle('Online Players')
+                .setColor(info.embedHexcode)
+                .setDescription(players)
+                .setTimestamp()
+                message.channel.send(playerEmbed);
+            }
+            
+            console.log(response);
+        });
+    }
+
+});
+ 
+client.login(config.token);
