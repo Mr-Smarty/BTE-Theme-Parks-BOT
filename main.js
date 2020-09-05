f35371c0ec27bbea879e4d522f1141c1ad977e81
Mr-Smarty <MeesterSmartey@gmail.com>
2020-09-05 00:43:12 -0400

    added jsonReader func to main, =req -> =score

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ const Enmap = require("enmap");
 
 console.log('starting...')
 
+function jsonReader(filePath, cb) {
+   fs.readFile(filePath, (err, fileData) => {
+       if (err) {
+           return cb && cb(err)
+       }
+       try {
+           const object = JSON.parse(fileData)
+           return cb && cb(null, object)
+       } catch(err) {
+           return cb && cb(err)
+       }
+   })
+}
+
 client.Discord = Discord;
 client.ping = ping;
 
